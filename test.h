ac867813fc4dbd4422bfce19ddb91497b5167785
Be Coder BD <asadujjamanalif24@gmail.com>
2024-05-11 18:55:01 +0600

    Add files via upload

---
+++ test.h
@@ -0,0 +1,81 @@
+<div class="post_ads">
+    <div class="img">
+        <a href="#"><img src="images/image (1).jpg" alt=""></a>
+    </div>
+    <div class="reguler_postinfo_section">
+        <div class="postinfo">
+            <h3> ₹ 5,500</h3>
+            <h5> Brass Copper Roman Soilder Chess Wooden Board Premium </h5>
+            <div class="loc_time">
+                <p>SAMUDRAPUR, MAHARASHTRA</p>
+                <p>6 DAYS AGO</p>
+            </div>
+        </div>
+    </div>
+    <div class="reguler_bagesection">
+        <div class="reguler_rightbage">
+            <i class="fa-regular fa-heart"></i>
+        </div>
+    </div>
+</div>
+<div class="post_ads">
+    <div class="img">
+        <a href="#"><img src="images/image (1).jpg" alt=""></a>
+    </div>
+    <div class="reguler_postinfo_section">
+        <div class="postinfo">
+            <h3> ₹ 5,500</h3>
+            <h5> Brass Copper Roman Soilder Chess Wooden Board Premium </h5>
+            <div class="loc_time">
+                <p>SAMUDRAPUR, MAHARASHTRA</p>
+                <p>6 DAYS AGO</p>
+            </div>
+        </div>
+    </div>
+    <div class="reguler_bagesection">
+        <div class="reguler_rightbage">
+            <i class="fa-regular fa-heart"></i>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+// .reguler_postinfo_section{
+
+
+
+.reguler_bagesection{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    position: absolute ;
+    top:5%;
+    left: 80%;
+    padding: 0px 10px 0px 10px ;
+}
+
+
+
+.reguler_rightbage{
+    height: 35px;
+    width: 35px;
+    background-color: #ffffff;
+    border-radius: 50%;
+}
+.reguler_rightbage i{
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.2rem;
+    color: #002F34;
+}
+
+
+
+
